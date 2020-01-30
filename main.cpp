dc0e73693a20480479d6bb8d1ce0c1fb4be7cc10
AxelCoutisson <axel.coutisson@hotmail.fr>
2020-01-30 19:26:50 +0100

    Question 8.a : Création du menu

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
+//Created by Axel Coutisson on 29 / 01 /2020
 #include <iostream>
+#include <string>
 #include <vector>
 #include "Product.h"
 #include "Client.h"
@@ -6,162 +8,253 @@
 #include "Store.h"
 using namespace std;
 
+//
+// Fonctions d'affichage
+//
+
+void afficheMenu()
+{
+	cout << endl << "**************" <<  endl << "* Easy Store *" <<  endl << "**************" <<  endl << "Enter the right number corresponding to your choose" << endl << "1. Manage Store" << endl << "2. Manage Users" << endl << "3. Manage Orders" << endl << "4. Exit" << endl;
+}
+
+void afficheMenuStore()
+{
+	cout << endl << "****************" <<  endl << "* Manage Store *" <<  endl << "****************" << endl <<  "Enter the right number corresponding to your choose" << endl << "1. Add product to the Store" << endl << "2. Display all products" << endl << "3. Search product by name" << endl << "4. Update product quantity" << endl << "5. return" << endl;
+}
+
+void afficheMenuUsers()
+{
+	cout << endl << "*****************" <<  endl << "* Manage Client *" <<  endl << "*****************" <<  endl << "Enter the right number corresponding to your choose" << endl << "1. Add client" << endl << "2. Display all clients" << endl << "3. Search client" << endl << "4. Search client by id" << endl << "5. Add product to basket" << endl << "6. Add product to basket by id" << endl << "7. remove product from basket" << endl << "8. Remove product from basket by id" << endl << "9. Modify product quantity in the basket" << endl << "10. Modify product quantity in the basket by id" << endl << "11. return" << endl;
+}
+
+void afficheMenuOrder()
+{
+	cout << endl << "*****************" <<  endl << "* Manage Orders *" <<  endl << "*****************" <<  endl << "Enter the right number corresponding to your choose" << endl << "1. Valideat client order" << endl << "2. Update order status" << endl << "3. Display all orders" << endl << "4. Display client orders" << endl << "5. Display client orders by id" << endl << "6. return" << endl;
+}
+
+//
+// Main
+//
+
 int main()
 {
-	
-	// Test special peter
-	// Store EasyStore;
-
-	// EasyStore.add_client("Bachour", "Peter");
-	// EasyStore.add_client("Coutisson", "Axel");
-
-	// EasyStore.add_product_to_store("PS4", "Console de jeu marque Sony", 10, 249.99);
-	// EasyStore.add_product_to_store("Switch", "Console de jeu Nintendo", 30,299.99);
-	// EasyStore.add_product_to_store("Xbox One", "Console de jeu Microsoft", 15,179.99);
-
-	// EasyStore.add_product_to_shopping_cart("PS4", 1);
-	// EasyStore.add_product_to_shopping_cart("PS4", 1);
-	// cout << "clients" << endl;
-	// EasyStore.display_clients();
-
-	// cout << endl << endl << "product" << endl;
-	// EasyStore.display_products();
-
-	/*
-  	//Question 1.
-  	cout << "Question 1 : Création de la classe Magasin." << endl;
-  	Store EasyStore;
-  	//Fin Question 1.   
-	*/
-	
-	/*
-  	//Question 2.
-  	cout << "Question 2 : Création de la classe Product." << endl;
-  	Product product("PS4", "Console de jeu marque Sony", 10, 249.99);
-  	cout << product << endl; 
-  	//Fin Question 2.
-	*/
-
-
-	/*
-	//Question 3.
-	cout << "Question 3 : Ajout de fonctionnalités à Magasin." << endl;
 	Store EasyStore;
-
-	// Add products to the store with Title, Description, Quantity and Price
-  	EasyStore.add_product_to_store("PS4", "Console de jeu Sony", 10,249.99);
-  	EasyStore.add_product_to_store("Switch", "Console de jeu Nintendo", 30,299.99);
-  	EasyStore.add_product_to_store("Xbox One", "Console de jeu Microsoft", 15,179.99);
-
-  	// Display all the products
-  	EasyStore.display_products();
-
-  	// Update quantity of a product
-  	EasyStore.update_product_quantity("Switch", 10);
-
-  	// Display the updated product
- 	EasyStore.display_product("Switch");
- 	*/
-
-	/*
-	//Question 4.
-	cout << "Question 4 : Création de la classe Client" << endl;
-	Client peter(0, "Bachour", "Peter");
-	Client axel(1, "Coutisson", "Axel");
-  	Product PS4("PS4", "Console de jeu marque Sony", 10, 249.99);
-	Product Switsh("Switch", "Console de jeu Nintendo", 30,299.99);
-	Product Xbox("Xbox One", "Console de jeu Microsoft", 15,179.99);
-
-	peter.add_product(&PS4);
-	peter.add_product(&Xbox);
-
-	axel.add_product(&Switsh);
-	axel.add_product(&PS4);
-
-  	cout <<  peter << endl;
-  	peter.modify_quantity("PS4", 3);
-  	cout <<  peter << endl;
-  	peter.delete_product("Xbox One");
-  	cout <<  peter << endl;
-  	peter.empty_basket();
-  	cout <<  peter << endl;
-  	cout << endl;
-  	cout << axel << endl;
-  	cout <<  PS4 << endl;
-  	cout << Switsh << endl;
-  	cout <<  Xbox << endl;
-  	*/
-
-	/*
-  	//Question 5.
-  	cout << "Question 5 : Ajout de fonctionnalités à Magasin." << endl;
-  	// Add new clients
-	EasyStore.add_client("Dom", "Ginhac");
-	EasyStore.add_client("Bill", "Gates");
-	EasyStore.add_client("Richard", "Stallman");
-	EasyStore.add_client("Alan", "Turing");
-	EasyStore.add_client("Steve", "Jobs");
-
-	// Display all the clients
-	EasyStore.display_clients();
-
-	// Display clients "Richard Stalman" and 4
-	EasyStore.display_client("Richard", "Stallman");
-	EasyStore.display_client(4);
-
-	// Add a product with its title and add to a client identified by his name
-	EasyStore.add_product_to_shopping_cart("PS4", "Richard", "Stallman");
-
-	// Add a product with its title and add to a client identified by his uid
-	EasyStore.add_product_to_shopping_cart("Xbox", 4);
-	*/
-
-	/*
-	//Question 6.
-	cout << "Question 6 : Création de la classe Commande." << endl;
-	Client peter(0, "Bachour", "Peter");
-  	Product PS4("PS4", "Console de jeu marque Sony", 10, 249.99);
-	Product Switsh("Switch", "Console de jeu Nintendo", 30,299.99);
-	Product Xbox("Xbox One", "Console de jeu Microsoft", 15,179.99);
-	peter.add_product(&PS4);
-	peter.add_product(&Switsh);
-	peter.add_product(&Xbox);
-    Order order(&peter, peter.get_productList());
-    cout << order << endl;
-    order.set_status(true);
-    cout << order << endl;
-   	*/
-	/*
-   	//Question 7.
-   	cout << "Question 7 : Ajout de fonctionnalités à Magasin." << endl;
-   	Store EasyStore;
-   	EasyStore.add_product_to_store("PS4", "Console de jeu Sony", 10,249.99);
-  	EasyStore.add_product_to_store("Switch", "Console de jeu Nintendo", 30,299.99);
-  	EasyStore.add_product_to_store("Xbox One", "Console de jeu Microsoft", 15,179.99);
-
-	EasyStore.add_client("Dom", "Ginhac");
-	EasyStore.add_client("Bill", "Gates");
-
-	EasyStore.add_product_to_shopping_cart("PS4", "Ginhac", "Dom");
-	EasyStore.add_product_to_shopping_cart("Xbox", "Ginhac", "Dom");
-
-	EasyStore.modify_quantity_in_shopping_cart("Xbox", "Ginhac", "Dom", 2);
-
-	EasyStore.validate_client_order("Ginhac", "Dom");
-	EasyStore.display_orders();
-
-	EasyStore.add_product_to_shopping_cart("Switch", "Gates", "Bill");
-	EasyStore.validate_client_order("Gates", "Bill");
-
-	EasyStore.display_orders_client("Gates","Bill");
-	EasyStore.display_products();
-	*/
-
-
-
-
-
-
-
+	int choose, subchoose;
+	bool stay = true, substay = true;
+	while(stay)
+	{
+		substay = true;
+		afficheMenu();
+		cin >> choose;
+		switch(choose)
+		{
+			case 1:
+				while(substay)
+				{
+					afficheMenuStore();
+					cin >>subchoose;
+					
+					if(subchoose == 1)
+					{
+						string title, description;
+						int quantity;
+						float price;
+						cout << endl << "title :" << endl;
+						cin >> title;
+						cout << endl << "description :" << endl;
+						cin >> description;
+						cout << endl << "quantity :" << endl;
+						cin >> quantity;
+						cout << endl << "price :" << endl;
+						cin >> price;
+						EasyStore.add_product_to_store(title,description,quantity,price);
+					}
+					if (subchoose == 2)
+					{
+						EasyStore.display_products();
+					}
+					if(subchoose == 3)
+					{
+						string product;
+						cout << endl << "Product name :" << endl;
+						cin >> product;
+						EasyStore.display_product(product);
+					}
+					if(subchoose == 4)
+					{
+						string product;
+						int quantity;
+						cout << endl << "Product name :" << endl;
+						cin >> product;
+						cout << endl << "Quantity :" << endl;
+						cin >> quantity;
+						EasyStore.update_product_quantity(product, quantity);
+					}
+					if(subchoose == 5)
+					{
+						substay = false;
+					}
+				}
+				break;
+			case 2:
+				while(substay)
+				{
+					afficheMenuUsers();
+					cin >>subchoose;
+					if(subchoose == 1)
+					{
+						string name, firstname;
+						cout << endl <<" Name :" << endl;
+						cin >> name;
+						cout << endl <<" Firstname :" << endl;
+						cin >> firstname;
+						EasyStore.add_client(name, firstname);
+					}
+					if(subchoose == 2)
+					{
+						EasyStore.display_clients();
+					}
+					if(subchoose == 3)
+					{
+						string name, firstname;
+						cout << endl <<" Name :" << endl;
+						cin >> name;
+						cout << endl <<" Firstname :" << endl;
+						cin >> firstname;
+						EasyStore.display_client(name, firstname);
+					}
+					if(subchoose == 4)
+					{
+						int id;
+						cout << endl <<" Client Id :" << endl;
+						cin >> id;
+						EasyStore.display_client(id);
+					}
+					if(subchoose == 5)
+					{
+						string producttitle, name, firstname;
+						cout << endl <<" Product Name :" << endl;
+						cin >> producttitle;
+						cout << endl <<" Name :" << endl;
+						cin >> name;
+						cout << endl <<" Firstname :" << endl;
+						cin >> firstname;
+						EasyStore.add_product_to_shopping_cart(producttitle,name,firstname);
+					}
+					if(subchoose == 6)
+					{
+						string producttitle;
+						int id;
+						cout << endl <<" Product Name :" << endl;
+						cin >> producttitle;
+						cout << endl <<" Client Id :" << endl;
+						cin >> id;
+						EasyStore.add_product_to_shopping_cart(producttitle,id);
+					}
+					if(subchoose == 7)
+					{
+						string producttitle, name, firstname;
+						cout << endl <<" Product Name :" << endl;
+						cin >> producttitle;
+						cout << endl <<" Name :" << endl;
+						cin >> name;
+						cout << endl <<" Firstname :" << endl;
+						cin >> firstname;
+						EasyStore.remove_product_from_shopping_cart(producttitle,name,firstname);
+					}
+					if(subchoose == 8)
+					{
+						string producttitle;
+						int id;
+						cout << endl <<" Product Name :" << endl;
+						cin >> producttitle;
+						cout << endl <<" Client Id :" << endl;
+						cin >> id;
+						EasyStore.remove_product_from_shopping_cart(producttitle,id);
+					}
+					if(subchoose == 9)
+					{
+						string producttitle, name, firstname;
+						int quantity;
+						cout << endl <<" Product Name :" << endl;
+						cin >> producttitle;
+						cout << endl <<" Name :" << endl;
+						cin >> name;
+						cout << endl <<" Firstname :" << endl;
+						cin >> firstname;
+						cout << endl <<" Quantity :" << endl;
+						cin >> quantity;
+						EasyStore.modify_quantity_in_shopping_cart(producttitle, name, firstname, quantity);
+					}
+					if(subchoose == 10)
+					{
+						string producttitle;
+						int id, quantity;
+						cout << endl <<" Product Name :" << endl;
+						cin >> producttitle;
+						cout << endl <<" Client Id:" << endl;
+						cin >> id;
+						cout << endl <<" quantity :" << endl;
+						cin >> quantity;
+						EasyStore.modify_quantity_in_shopping_cart(producttitle,id,quantity);
+					}
+					if(subchoose == 11)
+					{
+						substay = false;
+					}
+				}
+				break;
+			case 3:
+			while(substay)
+			{
+				afficheMenuOrder();
+				cin >>subchoose;
+				if(subchoose == 1)
+				{
+					string name, firstname;
+					cout << endl <<" Client Name :" << endl;
+					cin >> name;
+					cout << endl <<" Client Firstname :" << endl;
+					cin >> firstname;
+					EasyStore.validate_client_order(name,firstname);
+				}
+				if(subchoose == 2)
+				{
+					int order;
+					cout << endl <<" Order Id :" << endl;
+					cin >> order;
+					EasyStore.update_status_order(order);
+				}
+				if(subchoose == 3)
+				{
+					EasyStore.display_orders();
+				}
+				if(subchoose == 4)
+				{
+					string name, firstname;
+					cout << endl <<" Client name :" << endl;
+					cin >> name;
+					cout << endl <<" Client firstname :" << endl;
+					cin >> firstname;
+					EasyStore.display_orders_client(name, firstname);
+				}
+				if(subchoose == 5)
+				{
+					int id;
+					cout << endl <<" Client Id :" << endl;
+					cin >> id;
+					EasyStore.display_orders_client(id);
+				}
+				if(subchoose == 6)
+				{
+					substay = false;
+				}
+			}	
+				break;
+			case 4:
+				stay = false;
+				break;
+		}
+	}
 	return 0;
 }
